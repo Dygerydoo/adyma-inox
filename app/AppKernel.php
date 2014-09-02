a26a58c8248f977995b002a9234ddf9d6c6c6528
Fran Casero <caserofrancisco@gmail.com>
2014-09-02 15:36:42 +0200

    Added JMS Translation Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Adyma\MainBundle\AdymaMainBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new JMS\AopBundle\JMSAopBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
