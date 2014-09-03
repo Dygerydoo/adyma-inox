e9bd2adeeacddec30ebc4b203d11a8e4be57fdbc
Fran Casero <caserofrancisco@gmail.com>
2014-09-03 16:46:56 +0200

    Added sections to index page and created kc one landing and necessary archives

---
+++ src/Adyma/MainBundle/Resources/public/js/main.js
@@ -0,0 +1,14 @@
+// Trigger of socialbox and descriptionboxes
+$(document).ready(function(){
+     $(".trigger").mouseover(function(event){
+         $('#' + $(event.target).data('id')).toggleClass('slidemgin', 1200);
+            $("#st").toggleClass('socialteamove');
+     });
+});
+
+// Trigger of arrow animations
+$(document).ready(function () {
+    $(".trigger").on("mouseover", function(event) {
+        $(event.target).toggleClass('arrowhover');
+        });
+    });
